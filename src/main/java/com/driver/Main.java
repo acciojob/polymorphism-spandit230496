d2e37d6c084e92668099d35dbd7ed8e409fe29b1
spandit230496 <sandip.pandit230496@gmail.com>
2023-02-12 21:21:07 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+     static class  Product{
+         public static int product(int x,int y){
 
+         return x*y;}
+
+
+         public static int product(int x,int y,int z){
+
+             return x*y*z;}
+
+         public static double product(double x,double y){
+
+             return x*y;}
+     }
+public static void main (String args[]){
+
+ Product p=new Product();
+ p.product(5,6);
+ p.product(5,6,7);
+ p.product(5.3,3.2);
+
+}
 }
